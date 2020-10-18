c28a4c44be92440ac74c76b8f71e12297be2e42c
d1nsol <jinsik0126@gmail.com>
2020-10-18 20:53:30 +0900

    bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,27 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+});
+
+//save 전 수행됨
+userSchema.pre('save', function(next) {
+    const user = this;
+    if(user.isModified('password')) {
+        // 비밀번호를 암호화 시킨다.
+        // Salt 를 이용해서 비밀번호를 암호화 해야함
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
     }
 });
 
