698e1ed272678f3bebf34fc59f8a5ebc256f69dc
d1nsol <jinsik0126@gmail.com>
2020-10-19 10:43:51 +0900

    ===== nodejs end  =====

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -51,8 +52,50 @@ userSchema.pre('save', function(next) {
                 next();
             });
         });
+    } else {
+        next();
     }
 });
+
+userSchema.methods.comparePassword = function(plainPassword, callBackFunction) {
+    
+    // plainPassword 와 암호화된 비밀번호 비교
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return callBackFunction(err);
+        callBackFunction(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function(callBackFunction) {
+
+    const user = this;
+    
+    // jsonwebtoken 을 이용해서 token 을 생성하기
+    const token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return callBackFunction(err);
+        callBackFunction(null, user);
+    });
+    
+}
+
+userSchema.statics.findByToken = function(token, callBackFunction) {
+    const user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token 과 DB 에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) {
+                return callBackFunction(err);
+            }
+            callBackFunction(null, user);
+        });
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 
